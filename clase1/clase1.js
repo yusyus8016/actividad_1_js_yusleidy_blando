5b78cf4d0c452e6ba63c1993806bacc9c7882661
yusyus8016 <124086000+yusyus8016@users.noreply.github.com>
2023-08-01 19:41:58 -0500

    trabajo de clase

---
+++ clase1/clase1.js
@@ -0,0 +1,9 @@
+const name = "yusleidy "
+const lastname = " Blandon "
+const birthyear = " 36 años"
+const city = " medellin"
+
+
+    console.log('Hola mi nombre es ' + name  + lastname +'tengo ' +birthyear +' vivo en '  + city )
+
+
